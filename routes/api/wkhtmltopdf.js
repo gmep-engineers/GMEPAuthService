89c4e733e593823b480cb3cd135c292de6ccae6c
grenrock <stevengrenrock@live.com>
2025-07-11 09:22:38 -0700

    add site lighting tarrar

--- routes/api/wkhtmltopdf.js
+++ routes/api/wkhtmltopdf.js
@@ -159,4 +159,86 @@ router.post("/commercial", async function (req, res, next) {
   );
 });
 
+router.post("/site-lighting-tarrar", async (req, res) => {
+  var filename = "sit-lighting-tarrar-template.ejs";
+
+  var params = {};
+
+  params.NewConstruction = req.body.NewConstruction;
+
+  params.ProjectName = req.body.ProjectName;
+  params.ProjectAddress = req.body.ProjectAddress;
+  params.TarrarNo = req.body.TarrarNo;
+  params.DateSent = req.body.DateSent;
+
+  await new Promise((resolve, reject) => {
+    ejs.renderFile(
+      path.join(__dirname, "pdf_templates", filename),
+      params,
+      (err, str) => {
+        if (!err) {
+          fs.writeFileSync(
+            path.join(__dirname, "input", filename + templateId + ".html"),
+            str
+          );
+          filename = filename + templateId + ".html";
+          resolve();
+        } else {
+          console.log(err);
+          reject();
+        }
+      }
+    );
+  });
+
+  var footerTemplate = fs.readFileSync(
+    path.join(__dirname, "pdf_templates", "footer.html"),
+    "utf8"
+  );
+  footerTemplate = footerTemplate.replace("$ProjectName", req.body.ProjectName);
+  fs.writeFileSync(
+    path.join(__dirname, "input", `formatted-footer-${templateId}.html`),
+    footerTemplate
+  );
+  exec(
+    `wkhtmltopdf --enable-local-file-access --header-html "file://${path.join(
+      __dirname,
+      "pdf_templates",
+      "header.html"
+    )}" --footer-html "file://${path.join(
+      __dirname,
+      "input",
+      `formatted-footer-${templateId}.html`
+    )}" --page-size Letter -B 25.4mm "${path.join(
+      __dirname,
+      "input",
+      filename
+    )}" routes/api/output/${filename}.pdf`,
+    (err, stdout, stderr) => {
+      fs.unlinkSync(
+        path.join(__dirname, "input", `formatted-footer-${templateId}.html`)
+      );
+      fs.unlinkSync(path.join(__dirname, "input", filename));
+      if (err) {
+        console.log("wkhtmltopdf err");
+        console.error(err);
+        return res.status(500).send(err);
+      } else if (stderr && stderr.code) {
+        console.log("wkhtmltopdf stderr");
+        console.log(stderr.code);
+        console.error(stderr);
+        return res.status(500).send(stderr);
+      }
+      res.sendFile(path.join(__dirname, "output", filename + ".pdf"), (err) => {
+        if (err) {
+          console.log("sendFile stderr");
+          console.error(err);
+          return res.status(500).send(err);
+        }
+        fs.unlinkSync(`routes/api/output/${filename}.pdf`);
+      });
+    }
+  );
+});
+
 module.exports = router;
