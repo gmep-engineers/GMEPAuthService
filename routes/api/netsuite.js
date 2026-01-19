014b5757829bbcd92beea8028ad605d286645d00
grenrock <stevengrenrock@live.com>
2026-01-19 13:51:15 -0800

    create conn in router

--- routes/api/netsuite.js
+++ routes/api/netsuite.js
@@ -2,9 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 const { processSingleCustomer } = require("../../lib/customerMigration");
+const getSqlConnection = require("../../lib/getSqlConnection");
 
 router.post("/sync-client", async function (req, res, next) {
-  await processSingleCustomer(req.body.CustomerId, req.body.Token);
+  const conn = await getSqlConnection();
+  await processSingleCustomer(conn, req.body.CustomerId, req.body.Token);
+  conn.destroy();
   res.status(201).send({});
 });
 
