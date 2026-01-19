6d802786411952fcfa50c392e1d2a86d7d853524
grenrock <stevengrenrock@live.com>
2026-01-19 13:16:55 -0800

    remove default header definition

--- routes/api/netsuite.js
+++ routes/api/netsuite.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const { processSingleCustomer } = require("../../lib/customerMigration");
 
 router.post("/sync-client", async function (req, res, next) {
-  await processSingleCustomer(req.body.CustomerId, req.body.token);
+  await processSingleCustomer(req.body.CustomerId, req.body.Token);
   res.status(201).send({});
 });
 
