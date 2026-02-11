b39a6588138d7b8ef586e2fbce34a03bc5277f9a
grenrock <stevengrenrock@live.com>
2026-02-11 14:59:10 -0800

    initial changes for gemini app compatibility

--- routes/api/wkhtmltopdf.js
+++ routes/api/wkhtmltopdf.js
@@ -125,6 +125,226 @@ const formatPhone = function (phone) {
   }
 };
 
+const getGeminiParams = function (b) {
+  const params = {
+    NewConstruction: b.project_details.project_type === "New Construction",
+    ProjectName: b.project_details.name,
+    ProjectAddress: b.project_details.address,
+    Client: b.parties.client.business_name,
+    Architect: b.parties.architect.name,
+    DateSent: b.project_details.timeline.date_sent,
+    DateDrawingsReceived: b.project_details.timeline.date_drawings_received,
+    ProjectDescriptions: b.project_details.description,
+    TotalPrice: b.financial_provisions.total_fee.amount,
+    BudgetedAdminHours: b.administration_details.coordination_hours,
+    MaxAdminHours: b.administration_details.ca_hours,
+    ClientType: b.parties.client.loyalty_tier.toLowerCase(),
+    HasSiteVisit: b.project_details.features.site_visit,
+    RetainerPercent: b.financial_provisions.retainer_percentage,
+    ClientContactName: b.parties.client.contact_name,
+    ClientBusinessName: b.parties.client.business_name,
+    ClientStreetAddress: b.parties.client.address.street,
+    ClientCityStateZip: `${b.parties.client.address.city}, ${b.parties.client.address.state} ${b.parties.client.address.zip_code}`,
+    ClientEmail: b.parties.client.email,
+    ClientPhone: b.parties.client.phone,
+    NumMeetings: 0,
+    HasInitialRecommendationsMeeting: false,
+  };
+  return params;
+};
+
+const setDescriptionParams = function (projectDetails, params) {
+  const structuralDescriptions = projectDetails.structuralDescriptions;
+  const mechanicalDescriptions = projectDetails.mechanicalDescriptions;
+  const electricalDescriptions = projectDetails.electricalDescriptions;
+  const plumbingDescriptions = projectDetails.plumbingDescriptions;
+  var structuralDescriptionsStr = "";
+  var mechanicalDescriptionsStr = "";
+  var electricalDescriptionsStr = "";
+  var plumbingDescriptionsStr = "";
+
+  var includeStructural = false;
+  var includeMechanical = false;
+  var includeElectrical = false;
+  var includePlumbing = false;
+
+  var structuralKeys = Object.keys(structuralDescriptions);
+  for (let i of structuralKeys) {
+    if (structuralDescriptions[i]) {
+      includeStructural = true;
+    }
+  }
+  var mechanicalKeys = Object.keys(mechanicalDescriptions);
+  for (let i of mechanicalKeys) {
+    if (mechanicalDescriptions[i]) {
+      includeMechanical = true;
+    }
+  }
+  var electricalKeys = Object.keys(electricalDescriptions);
+  for (let i of electricalKeys) {
+    if (electricalDescriptions[i]) {
+      includeElectrical = true;
+    }
+  }
+  var plumbingKeys = Object.keys(plumbingDescriptions);
+  for (let i of plumbingKeys) {
+    if (plumbingDescriptions[i]) {
+      includePlumbing = true;
+    }
+  }
+
+  if (includeStructural) {
+    if (params.NewConstruction) {
+      structuralDescriptionsStr =
+        "New Construction: engineering for Structural design";
+    } else {
+      structuralDescriptionsStr =
+        "Tenant Improvement: engineering for Structural design";
+    }
+
+    if (structuralDescriptions.geotechnical_foundation_type) {
+      structuralDescriptionsStr +=
+        ", Review geotechnical report and define foundation type";
+    }
+    if (structuralDescriptions.structural_framing_depths) {
+      structuralDescriptionsStr +=
+        ", Perform structural analysis and design for all gravity and lateral load resisting elements";
+    }
+    if (structuralDescriptions.structural_analysis_design) {
+      structuralDescriptionsStr += ", Title 24";
+    }
+    if (structuralDescriptions.structural_plans) {
+      structuralDescriptionsStr += ", Structural Plans";
+    }
+    if (structuralDescriptions.details_and_calculations) {
+      structuralDescriptionsStr += ", Details and calculations";
+    }
+    if (structuralDescriptions.code_compliance) {
+      structuralDescriptionsStr += ", Building structural to comply with code";
+    }
+  }
+  if (includeMechanical) {
+    if (params.NewConstruction) {
+      mechanicalDescriptionsStr +=
+        "New Construction: engineering for Mechanical design";
+    } else {
+      mechanicalDescriptionsStr +=
+        "Tenant Improvement: engineering for HVAC design";
+    }
+    if (mechanicalDescriptions.exhaust_and_supply) {
+      mechanicalDescriptionsStr += ", exhaust and supply";
+    }
+    if (mechanicalDescriptions.hvac_equipment_specifications) {
+      mechanicalDescriptionsStr += ", HVAC equipment specifications";
+    }
+    if (mechanicalDescriptions.title_24) {
+      mechanicalDescriptionsStr += ", Title 24";
+    }
+  }
+
+  if (includeElectrical) {
+    if (params.NewConstruction) {
+      electricalDescriptionsStr +=
+        "New Construction: engineering for Electrical design";
+    } else {
+      electricalDescriptionsStr +=
+        "Tenant Improvement: engineering for Electrical design";
+    }
+    if (electricalDescriptions.electrical_power_design) {
+      electricalDescriptionsStr += ", Electrical power design";
+    }
+    if (electricalDescriptions.service_load_calculation) {
+      electricalDescriptionsStr += ", Service load calculation";
+    }
+    if (electricalDescriptions.single_line_diagram) {
+      electricalDescriptionsStr += ", Single line diagrams";
+    }
+    if (electricalDescriptions.electrical_lighting_design) {
+      electricalDescriptionsStr += ", Electrical lighting design";
+    }
+    if (electricalDescriptions.electrical_photometric) {
+      electricalDescriptionsStr += ", Electrical photometric";
+    }
+  }
+
+  if (includePlumbing) {
+    if (params.NewConstruction) {
+      plumbingDescriptionsStr +=
+        "New Construction: engineering for Plumbing design";
+    } else {
+      plumbingDescriptionsStr +=
+        "Tenant Improvement: engineering for Plumbing design";
+    }
+    if (plumbingDescriptions.hot_and_cold_water_piping) {
+      plumbingDescriptionsStr += ", Hot and cold water piping design";
+    }
+    if (plumbingDescriptions.sewer_and_vent_piping) {
+      plumbingDescriptionsStr += ", Sewer and vent piping design";
+    }
+  }
+
+  params.StructuralDescriptions = structuralDescriptionsStr;
+  params.MechanicalDescriptions = mechanicalDescriptionsStr;
+  params.ElectricalDescriptions = electricalDescriptionsStr;
+  params.PlumbingDescriptions = plumbingDescriptionsStr;
+};
+
+const setFunctionParams = function (params) {
+  params.getNextLetter = getNextLetter;
+
+  params.letter = "";
+  params.romanNumeral = "";
+  params.number = 1;
+
+  params.getNextRomanNumeral = getNextRomanNumeral;
+};
+
+const setScopeDepartmentListParams = function (params) {
+  var ScopeDepartmentList = [];
+
+  if (params.StructuralDescriptions) {
+    ScopeDepartmentList.push("structural");
+  }
+  if (params.MechanicalDescriptions) {
+    ScopeDepartmentList.push("mechanical");
+  }
+  if (params.ElectricalDescriptions) {
+    ScopeDepartmentList.push("electrical");
+  }
+  if (params.PlumbingDescriptions) {
+    ScopeDepartmentList.push("plumbing");
+  }
+
+  if (ScopeDepartmentList.length === 1) {
+    params.ScopeDepartmentList = ScopeDepartmentList[0];
+  } else if (ScopeDepartmentList.length === 2) {
+    params.ScopeDepartmentList = `${ScopeDepartmentList[0]} and ${ScopeDepartmentList[1]}`;
+  } else if (ScopeDepartmentList.length === 3) {
+    params.ScopeDepartmentList = `${ScopeDepartmentList[0]}, ${ScopeDepartmentList[1]}, and ${ScopeDepartmentList[2]}`;
+  } else if (ScopeDepartmentList.length === 4) {
+    params.ScopeDepartmentList = `${ScopeDepartmentList[0]}, ${ScopeDepartmentList[1]}, ${ScopeDepartmentList[2]}, and ${ScopeDepartmentList[3]}`;
+  }
+};
+
+router.post("/commercial2", async function (req, res, next) {
+  // accept the json from the gemini app
+  // port all the values over to ours
+  // save client information in the database if it isn't in the database yet
+  // generate pdf
+
+  const b = req.body;
+  const params = getGeminiParams(b);
+  setDescriptionParams(params);
+  setFunctionParams(params);
+  setScopeDepartmentListParams(params);
+  if (params.ScopeDepartmentList.length === 0) {
+    return res.status(400).send({ error: "scope department list empty" });
+  }
+  params.htmlPaymentScheduleTable = await paymentScheduleTable(params);
+
+  await finalize(req, res, filename, params);
+});
+
 router.post("/commercial", async function (req, res, next) {
   var filename = "commercial-proposal-template.ejs";
 
