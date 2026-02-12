e31f36180b581f3519d2478d161779c48f436c8f
grenrock <stevengrenrock@live.com>
2026-02-12 09:11:55 -0800

    add cors

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+const allowedOrigins = ["*"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // Enables passing of cookies/authorization headers
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // Specify allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+};
+
+app.use(cors(corsOptions));
+
 app.use("/api/user", userRouter);
 app.use("/api/session", sessionRouter);
 app.use("/api/wkhtmltopdf", wkhtmltopdfRouter);
